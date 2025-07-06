79a90a54c1073db81b569050bfcc1a42a61bf2e3
Leo Tovell <leobtovell@gmail.com>
2025-07-06 14:36:21 +0100

    Added pong to channel

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,16 +1,35 @@
-import { Command } from "./"
+import { Command } from "./";
 
 const command: Command = {
-    data: {
-        name: "ping",
-        description: "Ping the bot"
-    },
+  data: {
+    name: "ping",
+    description: "Ping the bot",
+  },
 
-    async execute(interaction) {
-        return {
-            content: "Pong, "+interaction.member.user.username+"!"
-        }
-    }
-}
+  async execute(interaction) {
+    // Get the channel to reply in:
+    const channelId = "1259574595388837958";
+    const botToken = process.env.TOKEN;
+    const username = interaction.member.user.username;
 
-export default command
+    await fetch(`https://discord.com/api/v10/channels/${channelId}/messages`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bot ${botToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: `Pong, ${username}!`,
+      }),
+    });
+
+    return {
+      type: 4,
+      data: {
+        content: `Sent Pong to <#${channelId}>.`,
+      },
+    };
+  },
+};
+
+export default command;
---
+++ src/commands/send-prompt.ts
@@ -0,0 +1,19 @@
+// import { Command } from "./";
+
+// const command: Command = {
+//   data: {
+//     name: "send-prompt",
+//     description: "[ADMIN] Sends the verification prompt to the specified channel",
+//   },
+
+//   async execute(interaction) {
+
+//     const channel = interaction.gu
+
+//     return {
+//       content: "Pong, " + interaction.member.user.username + "!",
+//     };
+//   },
+// };
+
+// export default command;
